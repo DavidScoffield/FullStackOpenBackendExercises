7215742ccf4faa7e2841af970c6fe23fb76ff56b
DavidScoffield <davidscoffield01@gmail.com>
2021-02-12 18:59:45 -0300

    4.2 Blog list, step2

---
+++ BlogList/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+
+logger.info('🔎🔎 connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info('✅✅ Connection sucefully')
+  })
+  .catch(() => {
+    logger.error('❌❌ Error in connection with the DB')
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
