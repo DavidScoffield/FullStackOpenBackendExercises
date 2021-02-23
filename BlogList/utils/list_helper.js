ebc6ee8be19ddaa8faeba5e92f074d9d6cfc50ac
DavidScoffield <davidscoffield01@gmail.com>
2021-02-23 19:20:31 -0300

    4.5

--- BlogList/utils/list_helper.js
+++ BlogList/utils/list_helper.js
@@ -2,15 +2,34 @@ const dummy = (blogs) => {
   return 1
 }
 
+const favoriteBlog = (blogs) => {
+  const findBlogWithMoreLikes = (blogWithMoreLikes, actualBlog) => {
+    blogWithMoreLikes = blogWithMoreLikes || actualBlog
+    return blogWithMoreLikes.likes >= actualBlog.likes ? blogWithMoreLikes : actualBlog
+  }
+
+  const selectFormattedFavoriteBlog = () => {
+    let blog = blogs.reduce(findBlogWithMoreLikes, {})
+    return {
+      title: blog.title,
+      author: blog.author,
+      likes: blog.likes,
+    }
+  }
+
+  return blogs.length ? selectFormattedFavoriteBlog() : {}
+}
+
 const totalLikes = (blogs) => {
   const sumAllLikes = (sum, items) => {
     return sum + items.likes
   }
 
-  return blogs.reduce(sumAllLikes, 0)
+  return blogs.length ? blogs.reduce(sumAllLikes, 0) : 0
 }
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
